db6bea3ac7bb232b6dd67bf18de25242bc8d7b24
Leonel-Rachid10 <leonelracgid10@gmail.com>
2025-10-27 00:13:44 +0200

    modo escuro

---
+++ exercice/pessoal/script.js
@@ -0,0 +1,78 @@
+
+ 
+
+   {string} elementId 
+function toggleDetails(elementId) {
+   
+    const detailsDiv = document.getElementById(elementId);
+
+   
+
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    const btnTopo = document.getElementById("btnVoltarTopo");
+    
+    
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        btnTopo.classList.remove("hidden");
+    } else {
+       
+        btnTopo.classList.add("hidden");
+    }
+}
+    
+    
+    const button = event.target; 
+
+    
+    detailsDiv.classList.toggle('hidden');
+
+    
+    if (detailsDiv.classList.contains('hidden')) {
+       
+        button.textContent = 'Mostrar Detalhes';
+    } else {
+        
+        button.textContent = 'Esconder Detalhes';
+    }
+
+
+const toggleButton = document.getElementById('toggleDarkMode');
+const body = document.body;
+const darkThemeKey = 'isDarkTheme'; 
+function applyTheme(isDark) {
+    if (isDark) {
+        body.classList.add('dark-theme');
+        
+        toggleButton.innerHTML = '<i class="fas fa-sun"></i>';
+    } else {
+        body.classList.remove('dark-theme');
+        
+        toggleButton.innerHTML = '<i class="fas fa-moon"></i>';
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const savedTheme = localStorage.getItem(darkThemeKey);
+    const isDark = savedTheme === 'true'; 
+
+    applyTheme(isDark);
+});
+
+
+toggleButton.addEventListener('click', () => {
+    
+    const isCurrentlyDark = body.classList.contains('dark-theme');
+    
+    
+    applyTheme(!isCurrentlyDark);
+
+    localStorage.setItem(darkThemeKey, !isCurrentlyDark);
+});
+}
