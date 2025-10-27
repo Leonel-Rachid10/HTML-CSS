a3c2889c34824a7fdffa57ac105af6b01c8b1495
Leonel-Rachid10 <leonelracgid10@gmail.com>
2025-10-27 10:50:48 +0200

    novas funcionalidades

--- exercice/pessoal/script.js
+++ exercice/pessoal/script.js
@@ -1,12 +1,55 @@
 
- 
 
-   {string} elementId 
+const toggleButton = document.getElementById('toggleDarkMode');
+const body = document.body;
+const darkThemeKey = 'isDarkTheme'; 
+
+function applyTheme(isDark) {
+    if (isDark) {
+        body.classList.add('dark-theme');
+        
+        toggleButton.innerHTML = '<i class="fas fa-sun"></i>';
+    } else {
+        body.classList.remove('dark-theme');
+       
+        toggleButton.innerHTML = '<i class="fas fa-moon"></i>';
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+   
+    const savedTheme = localStorage.getItem(darkThemeKey);
+    const isDark = savedTheme === 'true'; 
+    applyTheme(isDark);
+});
+
+toggleButton.addEventListener('click', () => {
+  
+    const isCurrentlyDark = body.classList.contains('dark-theme');
+    
+    
+    applyTheme(!isCurrentlyDark);
+
+    
+    localStorage.setItem(darkThemeKey, !isCurrentlyDark);
+});
+
+
+
 function toggleDetails(elementId) {
-   
     const detailsDiv = document.getElementById(elementId);
-
    
+    const button = event.target; 
+
+    detailsDiv.classList.toggle('hidden');
+
+    if (detailsDiv.classList.contains('hidden')) {
+        button.textContent = 'Mostrar Detalhes';
+    } else {
+        button.textContent = 'Esconder Detalhes';
+    }
+}
 
 
 window.onscroll = function() {
@@ -15,64 +58,11 @@ window.onscroll = function() {
 
 function scrollFunction() {
     const btnTopo = document.getElementById("btnVoltarTopo");
-    
-    
+  
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         btnTopo.classList.remove("hidden");
     } else {
-       
+    
         btnTopo.classList.add("hidden");
     }
-}
-    
-    
-    const button = event.target; 
-
-    
-    detailsDiv.classList.toggle('hidden');
-
-    
-    if (detailsDiv.classList.contains('hidden')) {
-       
-        button.textContent = 'Mostrar Detalhes';
-    } else {
-        
-        button.textContent = 'Esconder Detalhes';
-    }
-
-
-const toggleButton = document.getElementById('toggleDarkMode');
-const body = document.body;
-const darkThemeKey = 'isDarkTheme'; 
-function applyTheme(isDark) {
-    if (isDark) {
-        body.classList.add('dark-theme');
-        
-        toggleButton.innerHTML = '<i class="fas fa-sun"></i>';
-    } else {
-        body.classList.remove('dark-theme');
-        
-        toggleButton.innerHTML = '<i class="fas fa-moon"></i>';
-    }
-}
-
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    const savedTheme = localStorage.getItem(darkThemeKey);
-    const isDark = savedTheme === 'true'; 
-
-    applyTheme(isDark);
-});
-
-
-toggleButton.addEventListener('click', () => {
-    
-    const isCurrentlyDark = body.classList.contains('dark-theme');
-    
-    
-    applyTheme(!isCurrentlyDark);
-
-    localStorage.setItem(darkThemeKey, !isCurrentlyDark);
-});
 }
